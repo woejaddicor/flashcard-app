40b437365021fc0c044065518716691b9b1a6158
MHaroonNasir <hrn-nsr@hotmail.com>
2023-04-17 11:58:33 +0100

    add a logo to navbar

--- flashcard-app/src/components/NavBar/index.jsx
+++ flashcard-app/src/components/NavBar/index.jsx
@@ -1,5 +1,11 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from '../assets/logo.png'
+
+const logoStyle = {
+    width: "50px",
+    height: "50px"
+}
 
 export default function NavBar() {
     return (
@@ -7,7 +13,8 @@ export default function NavBar() {
             <header>
                 <Navbar bg="dark" variant="dark">
                     <Container>
-                        <Navbar.Brand href="/">Ghost</Navbar.Brand>
+                        <Navbar.Brand href="/"><img src={logo} 
+                        style={logoStyle}/></Navbar.Brand>
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/login">Login</Nav.Link>
                         <Nav.Link href="/dashboard">Dashboard</Nav.Link>
