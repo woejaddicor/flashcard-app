94c71e8388f697bafe5d10446087cc78bf3be5be
sk.ahm <itzshakeey@gmail.com>
2023-04-17 17:15:36 +0100

    minor change

--- flashcard-app/src/pages/FrenchPage/index.jsx
+++ flashcard-app/src/pages/FrenchPage/index.jsx
@@ -2,39 +2,39 @@ import React, { useState, useEffect } from "react";
 import { FlashcardNavigator, Flashcard } from "../../components";
 
 export default function FrenchPage() {
-  const [phrases, setPhrases] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [phrases, setPhrases] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  async function fetchData() {
-    const response = await fetch("/phrases.json");
-    const data = await response.json();
-    setPhrases(data.french);
-  }
+    async function fetchData() {
+        const response = await fetch("/phrases.json");
+        const data = await response.json();
+        setPhrases(data.french);
+    }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  function goToNextPhrase() {
-    setCurrentIndex((currentIndex + 1) % phrases.length);
-  }
+    function goToNextPhrase() {
+        setCurrentIndex((currentIndex + 1) % phrases.length);
+    }
 
-  function goToPreviousPhrase() {
-    setCurrentIndex((currentIndex - 1 + phrases.length) % phrases.length);
-  }
+    function goToPreviousPhrase() {
+        setCurrentIndex((currentIndex - 1 + phrases.length) % phrases.length);
+    }
 
-  return (
-    <div>
-      <h1>French Page</h1>
-      {phrases.length > 0 && (
-        <Flashcard phrase={phrases[currentIndex]} />
-      )}
-      <FlashcardNavigator
-        currentIndex={currentIndex}
-        phrases={phrases}
-        onPreviousClick={goToPreviousPhrase}
-        onNextClick={goToNextPhrase}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <h1>French Page</h1>
+            {phrases.length > 0 && (
+                <Flashcard phrase={phrases[currentIndex]} />
+            )}
+            <FlashcardNavigator
+                currentIndex={currentIndex}
+                phrases={phrases}
+                onPreviousClick={goToPreviousPhrase}
+                onNextClick={goToNextPhrase}
+            />
+        </div>
+    );
 }
