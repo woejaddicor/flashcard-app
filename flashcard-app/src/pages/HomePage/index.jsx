5a3d7f2e3a71c29370196ee8c878823831e8f50f
woejaddicor <your@emailadd.com>
2023-04-18 10:48:46 +0100

    homepage html and css changes

--- flashcard-app/src/pages/HomePage/index.jsx
+++ flashcard-app/src/pages/HomePage/index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import banner from './My project-1.png'
+import './homepage.css'
 
 export default function HomePage() {
     return(
-        <div>
-            <h1>Home Page</h1>
+        <>
+        <div className="page-banner">
+            <img className="banner-image" src={banner} alt="banner image"/>
+            <h1>Welcome to Crammer!</h1>
         </div>
+        <div>
+            <p>Welcome to Crammer, the ultimate revision website designed to help you in your language learning process!</p>
+            <Link to="/register"><button className="account-button">CREATE AN ACCOUNT</button></Link>
+            <h2>Learn</h2>
+        </div>
+        <div className="buttons-container">
+            <Link to="/french" className="language-links">
+                French 🇫🇷
+            </Link>
+            <Link to="/spanish" className="language-links">
+               Spanish 🇪🇸
+            </Link>
+        </div>
+        </>
     )
 }
