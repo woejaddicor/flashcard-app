79a458e5640d2427ca1cc4120f7bd439ade307cb
woejaddicor <your@emailadd.com>
2023-04-18 14:08:19 +0100

    latest aboutus page progresss

--- flashcard-app/src/pages/AboutUsPage/index.jsx
+++ flashcard-app/src/pages/AboutUsPage/index.jsx
@@ -5,6 +5,7 @@ export default function AboutUsPage() {
   const [isAccordion1Open, setIsAccordion1Open] = useState(false);
   const [isAccordion2Open, setIsAccordion2Open] = useState(false);
   const [isAccordion3Open, setIsAccordion3Open] = useState(false);
+  const [isAccordion4Open, setIsAccordion4Open] = useState(false);
 
   const toggleAccordion1 = () => {
     setIsAccordion1Open(!isAccordion1Open);
@@ -18,33 +19,49 @@ export default function AboutUsPage() {
     setIsAccordion3Open(!isAccordion3Open);
   };
 
+  const toggleAccordion4 = () => {
+    setIsAccordion4Open(!isAccordion4Open);
+  };
+
   return (
     <div>
       <h1>About Us</h1>
       <div className="accordion-container">
-        <div className="accordion" onClick={toggleAccordion1}>
-          <h2 style={{backgroundColor: "#14293D"}}>Our Story</h2>
-          {isAccordion1Open && (
-            <div className="accordion-content">
-              <p>Test</p>
-            </div>
-          )}
+        <div className="grid-container">
+          <div className="accordion" onClick={toggleAccordion1}>
+            <h2 style={{backgroundColor: "#006992"}}>Our Story</h2>
+            {isAccordion1Open && (
+              <div className="accordion-content">
+                <p>Test</p>
+              </div>
+            )}
+          </div>
+          <div className="accordion" onClick={toggleAccordion2}>
+            <h2 style={{backgroundColor: "#006992"}}>About This App</h2>
+            {isAccordion2Open && (
+              <div className="accordion-content">
+                <p>Test</p>
+              </div>
+            )}
+          </div>
         </div>
-        <div className="accordion" onClick={toggleAccordion2}>
-          <h2 style={{backgroundColor: "#14293D"}}>About This App</h2>
-          {isAccordion2Open && (
-            <div className="accordion-content">
-              <p>Test</p>
-            </div>
-          )}
-        </div>
-        <div className="accordion" onClick={toggleAccordion3}>
-          <h2 style={{backgroundColor: "#14293D"}}>Contact Us</h2>
-          {isAccordion3Open && (
-            <div className="accordion-content">
-              <p>Test</p>
-            </div>
-          )}
+        <div className="grid-container">
+          <div className="accordion" onClick={toggleAccordion3}>
+            <h2 style={{backgroundColor: "#006992"}}>Contact Us</h2>
+            {isAccordion3Open && (
+              <div className="accordion-content">
+                <p>Test</p>
+              </div>
+            )}
+          </div>
+          <div className="accordion" onClick={toggleAccordion4}>
+            <h2 style={{backgroundColor: "#006992"}}>Fourth Item</h2>
+            {isAccordion4Open && (
+              <div className="accordion-content">
+                <p>Test</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
