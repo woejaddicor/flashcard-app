29223e866a6aff2232433ba9ce32810b3fe1d4db
sk.ahm <itzshakeey@gmail.com>
2023-04-18 17:45:44 +0100

    completed scoreboard

--- flashcard-app/src/components/Score/index.jsx
+++ flashcard-app/src/components/Score/index.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Score({ score, totalQuestions, flaggedQuestions }) {
+    const [clickedQuestions, setClickedQuestions] = useState([]);
+
     const percentage = ((score / totalQuestions) * 100).toFixed(2);
+
+    function handleQuestionClick(index) {
+        if (clickedQuestions.includes(index)) {
+            setClickedQuestions((prevClickedQuestions) =>
+                prevClickedQuestions.filter((clickedIndex) => clickedIndex !== index)
+            );
+        } else {
+            setClickedQuestions((prevClickedQuestions) => [
+                ...prevClickedQuestions,
+                index,
+            ]);
+        }
+    }
 
     return (
         <div>
             <h2>Score: {percentage}%</h2>
             <h3>Flagged Questions:</h3>
             {flaggedQuestions.map((item, index) => (
-                <div key={index}>
-                    <p>Question: {item.question} Answer: {item.answer}</p>
-
+                <div key={index} onClick={() => handleQuestionClick(index)}>
+                    <p>{clickedQuestions.includes(index) ? item.answer : item.question}</p>
                 </div>
             ))}
         </div>
