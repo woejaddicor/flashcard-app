d3bd75e2bdc060512e7196edde714c33833126d8
sk.ahm <itzshakeey@gmail.com>
2023-04-18 12:48:28 +0100

    formatting minor change

--- flashcard-app/src/pages/FrenchPage/index.jsx
+++ flashcard-app/src/pages/FrenchPage/index.jsx
@@ -2,46 +2,46 @@ import React, { useState, useEffect } from "react";
 import { FlashcardNavigator, Flashcard } from "../../components";
 
 export default function FrenchPage() {
-  const [phrases, setPhrases] = useState([]);
-  const [index, setIndex] = useState(1);
+    const [phrases, setPhrases] = useState([]);
+    const [index, setIndex] = useState(1);
 
-  async function fetchData() {
-    const response = await fetch(
-      `https://crammer-backend.onrender.com/french/${index}`
+    async function fetchData() {
+        const response = await fetch(
+            `https://crammer-backend.onrender.com/french/${index}`
+        );
+        const data = await response.json();
+        setPhrases(data.question);
+    }
+
+    function handlePreviousClick() {
+        if (index > 1) {
+            setIndex(index - 1);
+        }
+    }
+
+    function handleNextClick() {
+        if (index < 15) {
+            setIndex(index + 1);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [index]);
+
+    return (
+        <div>
+            <h1>French Page</h1>
+            <FlashcardNavigator
+                currentIndex={index}
+                onPreviousClick={handlePreviousClick}
+                onNextClick={handleNextClick}
+                disableNext={index === 15} // disable next button if index is 15
+            >
+                {phrases.map((phrase) => (
+                    <Flashcard key={phrase.id} phrase={phrase} />
+                ))}
+            </FlashcardNavigator>
+        </div>
     );
-    const data = await response.json();
-    setPhrases(data.question);
-  }
-
-  function handlePreviousClick() {
-    if (index > 1) {
-      setIndex(index - 1);
-    }
-  }
-
-  function handleNextClick() {
-    if (index < 15) {
-      setIndex(index + 1);
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, [index]);
-
-  return (
-    <div>
-      <h1>French Page</h1>
-      <FlashcardNavigator
-        currentIndex={index}
-        onPreviousClick={handlePreviousClick}
-        onNextClick={handleNextClick}
-        disableNext={index === 15} // disable next button if index is 15
-      >
-        {phrases.map((phrase) => (
-          <Flashcard key={phrase.id} phrase={phrase} />
-        ))}
-      </FlashcardNavigator>
-    </div>
-  );
 }
