a70ee189a469ede358b9effe914970c8dabfa9e4
sk.ahm <itzshakeey@gmail.com>
2023-04-18 09:49:39 +0100

    add spanish and french link to dashboard

--- flashcard-app/src/pages/DashboardPage/index.jsx
+++ flashcard-app/src/pages/DashboardPage/index.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect } from "react";
 import { useToken } from "../../contexts";
 import LoginPage from "../LoginPage";
+import { Link } from "react-router-dom";
 
-export default function DashboardPage({token, setToken}) {
+export default function DashboardPage({ token, setToken }) {
 
-    console.log("tokendash", token)
-    if(!token) {
-        console.log("not token", token)
-        return <LoginPage token={token} setToken={setToken}/>
-    }
+    // console.log("tokendash", token)
+    // if(!token) {
+    //     console.log("not token", token)
+    //     return <LoginPage token={token} setToken={setToken}/>
+    // }
+    
 
-    return(
+    return (
         <div>
             <h1>Dashboard Page</h1>
+            <Link to="/french">French</Link>
+            <Link to="/spanish">Spanish</Link>
         </div>
-    )
+    );
 }
+
+
