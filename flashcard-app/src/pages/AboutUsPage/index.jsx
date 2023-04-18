141ea98e35e0aa90d2262b9204b96b1636332f18
woejaddicor <your@emailadd.com>
2023-04-18 12:36:43 +0100

    progress on about us with accordion elements

--- flashcard-app/src/pages/AboutUsPage/index.jsx
+++ flashcard-app/src/pages/AboutUsPage/index.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import './aboutUs.css'
 
 export default function AboutUsPage() {
-    return(
-        <div>
-            <h1>About Us Page</h1>
+  const [isAccordion1Open, setIsAccordion1Open] = useState(false);
+  const [isAccordion2Open, setIsAccordion2Open] = useState(false);
+  const [isAccordion3Open, setIsAccordion3Open] = useState(false);
+
+  const toggleAccordion1 = () => {
+    setIsAccordion1Open(!isAccordion1Open);
+  };
+
+  const toggleAccordion2 = () => {
+    setIsAccordion2Open(!isAccordion2Open);
+  };
+
+  const toggleAccordion3 = () => {
+    setIsAccordion3Open(!isAccordion3Open);
+  };
+
+  return (
+    <div>
+      <h1>About Us</h1>
+      <div className="accordion-container">
+        <div className="accordion" onClick={toggleAccordion1}>
+          <h2 style={{backgroundColor: "#14293D"}}>Our Story</h2>
+          {isAccordion1Open && (
+            <div className="accordion-content">
+              <p>Test</p>
+            </div>
+          )}
         </div>
-    )
+        <div className="accordion" onClick={toggleAccordion2}>
+          <h2 style={{backgroundColor: "#14293D"}}>About This App</h2>
+          {isAccordion2Open && (
+            <div className="accordion-content">
+              <p>Test</p>
+            </div>
+          )}
+        </div>
+        <div className="accordion" onClick={toggleAccordion3}>
+          <h2 style={{backgroundColor: "#14293D"}}>Contact Us</h2>
+          {isAccordion3Open && (
+            <div className="accordion-content">
+              <p>Test</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
