3f3dc065da653101ccccc5079e5115a2b7b5d3a5
woejaddicor <your@emailadd.com>
2023-04-19 10:06:41 +0100

    Added reveal flashcard and delete flashcard functions

--- flashcard-app/src/pages/FlashcardView/index.jsx
+++ flashcard-app/src/pages/FlashcardView/index.jsx
@@ -1,23 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import LoginPage from "../LoginPage";
+import './flashcardview.css'
 
-export default function FlashcardView({token, setToken}) {
+export default function FlashcardView({ token, setToken }) {
+  const [flashcards, setFlashcards] = useState([]);
+  const [showCard, setShowCard] = useState({});
+  const [deletedCardId, setDeletedCardId] = useState('')
 
-    if(!token) {
-        return <LoginPage token={token} setToken={setToken}/>
-    }
+  if (!token) {
+    return <LoginPage token={token} setToken={setToken} />;
+  }
 
-    return(
-        <div>
-            <h1>Flashcard View Page</h1>
-            <div className="banner">
-                <h3>Subject</h3>
-                <h3>Word / Phrase</h3>
-                <h3>Answer</h3>
-                <h3>Action</h3>
-            </div>
-            <button onClick={() => {location.href="/dashboard"}}>
-                Back to Dashboard</button>
-        </div>
-    )
+  useEffect(() => {
+    const fetchFlashcards = async () => {
+      try {
+        const response = await fetch(
+          "https://crammer-backend.onrender.com/custom"
+        );
+        const flashcardData = await response.json();
+        setFlashcards(flashcardData.questions);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchFlashcards();
+  }, []);
+  
+  function deleteFlashcard(id, type) {
+    fetch(`https://crammer-backend.onrender.com/${type}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+    })
+    .then(response => {
+      if (response.ok) {
+        window.location.reload();
+      } else {
+        throw new Error('Delete request failed');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+  
+  
+  
+  return ( 
+    <div className="flashcard-grid">
+      {flashcards && Array.isArray(flashcards) &&
+        flashcards.map((flashcard) => (
+          <div className="flashcard" key={flashcard.id}>
+              {showCard[flashcard.id] ? <p className="flashcard-answer">{flashcard.answer}</p> : <p className="flashcard-question">{flashcard.word_phrase}</p>}
+              <button onClick={() => handleCardClick(flashcard.id)}>Reveal Answer</button>
+              <button onClick={() => deleteFlashcard(flashcard.id, flashcard.type)}>Delete</button>
+          </div>
+        ))}
+    </div>
+  );
 }
