e1ff6b6c1852b83edd99497b9d85074be93310fd
woejaddicor <your@emailadd.com>
2023-04-19 14:15:50 +0100

    css changes to create card page

--- flashcard-app/src/pages/FlashcardCreate/index.jsx
+++ flashcard-app/src/pages/FlashcardCreate/index.jsx
@@ -47,21 +47,22 @@ export default function FlashcardCreate({token, setToken}) {
 
     return(
         <div className="create-page">
-            <h1>Create a new flashcard</h1>
+            <h1 className="page-title">Create a new flashcard</h1>
             <form onSubmit={handleSubmit}>
-                <h3>Subject</h3>
+                <h3 className="page-subtitles">Subject</h3>
                 <select  className="language-dropdown" value={selectedOption} onChange={handleChange}>
                     <option defaultValue={selectedOption}>Select a language</option>
                     <option value="French">French</option>
                     <option value="Spanish">Spanish</option>
                 </select>
-                <h3>Word / Phrase</h3>
-                <input className="input-boxes" type="text" name="words" placeholder="Example: Hello" value={wordText} onChange={handleWordChange}/>
-                <h3>Answer</h3>
-                <input  className="input-boxes" type="text" name="answers" placeholder="Example: Ola" value={answerText} onChange={handleAnswerChange}/>
-                <button onClick={handleSubmit} type="submit">Create Flashcard</button>
+                <h3 className="page-subtitles">Word / Phrase</h3>
+                <input className="input-boxes" type="text" name="words" placeholder="Example: Hello" value={wordText} onChange={handleWordChange} required/>
+                <h3 className="page-subtitles">Answer</h3>
+                <input  className="input-boxes" type="text" name="answers" placeholder="Example: Ola" value={answerText} onChange={handleAnswerChange} required/>
+                <br></br>
+                <button className="submit-button" onClick={handleSubmit} type="submit">Create Flashcard</button>
             </form>
-            <button onClick={() => {location.href="/dashboard"}}>
+            <button className="submit-button" onClick={() => {location.href="/dashboard"}}>
                 Back to Dashboard</button>
             <h2>{newQuestionSubmitted ? 'Submitted' : ''}</h2>
         </div>
