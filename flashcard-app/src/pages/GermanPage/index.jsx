4ce57b4950c18b435356b3f490e2d8e97714d5a3
sk.ahm <itzshakeey@gmail.com>
2023-04-20 11:44:51 +0100

    add the german page and update path to flashcardStyles

--- flashcard-app/src/pages/GermanPage/index.jsx
+++ flashcard-app/src/pages/GermanPage/index.jsx
@@ -1,15 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FlashcardNavigator, Flashcard, Score } from "../../components";
 import LoginPage from "../LoginPage";
+import '../../assets/flashcardStyles.css';
+
+const totalQuestions = 15;
 
 export default function GermanPage({ token, setToken }) {
+    const [question, setQuestion] = useState([]);
+    const [index, setIndex] = useState(1);
+    const [quizFinished, setQuizFinished] = useState(false);
+    const [wrongAnswers, setWrongAnswers] = useState([]);
+    const [clickedIndices, setClickedIndices] = useState([]);
 
-    if(!token) {
-        return <LoginPage token={token} setToken={setToken}/>
+    if (!token) {
+        return <LoginPage token={token} setToken={setToken} />
     }
 
-    return(
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(`https://crammer-backend.onrender.com/german/${index}`);
+            const data = await response.json();
+            console.log(data)
+            setQuestion(data.question);
+        }
+        fetchData();
+    }, [index]);
+
+
+    function handlePreviousClick() {
+        setIndex((prevIndex) => (prevIndex > 1 ? prevIndex - 1 : prevIndex));
+    }
+
+    function handleNextClick() {
+        setIndex((prevIndex) => (prevIndex < totalQuestions ? prevIndex + 1 : prevIndex));
+    }
+
+    function handleFinishClick() {
+        setQuizFinished(true);
+    }
+
+    function handleWrongClick() {
+        const questionText = question.map((phrase) => phrase.question).join(" ");
+        const answerText = question.map((phrase) => phrase.answer).join(" ");
+        setWrongAnswers((prevWrongAnswers) => [
+            ...prevWrongAnswers,
+            { question: questionText, answer: answerText },
+        ]);
+        setClickedIndices((prevClickedIndices) => [...prevClickedIndices, index]);
+    }
+
+    if (quizFinished) {
+        return (
+            <Score
+                score={totalQuestions - wrongAnswers.length}
+                totalQuestions={totalQuestions}
+                flaggedQuestions={wrongAnswers}
+            />
+        );
+    }
+
+    return (
         <div>
-            <h1>German Page</h1>
+            <h1> Translate the word</h1>
+            <FlashcardNavigator
+                currentIndex={index}
+                onPreviousClick={handlePreviousClick}
+                onNextClick={handleNextClick}
+                totalQuestions={totalQuestions}
+                isLastQuestion={index === totalQuestions}
+            >
+                {question.map((phrase) => (
+                    <Flashcard key={phrase.id} phrase={phrase} />
+                ))}
+                <button className="wrong-btn" onClick={handleWrongClick} disabled={clickedIndices.includes(index)}>Wrong</button>
+
+                {index === totalQuestions && (
+                    <button className="finish-btn" onClick={handleFinishClick}>
+                        Finish
+                    </button>
+                )}
+            </FlashcardNavigator>
+
+
         </div>
-    )
+    );
 }
