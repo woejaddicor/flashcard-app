83a3494d0f420c0e9a4f9f3d4b6bd7e3bf9185f4
sk.ahm <itzshakeey@gmail.com>
2023-04-20 22:33:13 +0100

    turn instructions into component

--- flashcard-app/src/pages/FrenchPage/index.jsx
+++ flashcard-app/src/pages/FrenchPage/index.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { FlashcardNavigator, Flashcard, Score } from "../../components";
+import { FlashcardNavigator, Flashcard, Score, Instructions } from "../../components";
 import LoginPage from "../LoginPage";
 import '../../assets/flashcardStyles.css';
-import Button from 'react-bootstrap/button';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Popover from 'react-bootstrap/Popover';
+
 
 export default function FrenchPage({ testing = false, token, setToken }) {
     const [question, setQuestion] = useState([]);
@@ -18,20 +16,6 @@ export default function FrenchPage({ testing = false, token, setToken }) {
         return <LoginPage token={token} setToken={setToken} />
     }
 
-    const popover = (
-        <Popover id="popover-basic" >
-            <Popover.Header as="h3">How to play:</Popover.Header>
-            <Popover.Body>
-                To play the game <strong style={{ color: "#ECA400" }}>hover over the blue card with the English text</strong>. When you think you know the correct translation, click the card to flip it over and see. <strong style={{ color: "#ECA400" }}>Click either the 'right' or 'wrong' button depending on the outcome</strong> and your score will total up at the end.
-            </Popover.Body>
-        </Popover>
-    );
-
-    const Instructions = () => (
-        <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-            <Button className="instructions-button" variant="success">Instructions</Button>
-        </OverlayTrigger>
-    );
 
     useEffect(() => {
         async function fetchData() {
