bff8324aaf83d5855c126b8c56bd5b506ff43339
sk.ahm <itzshakeey@gmail.com>
2023-04-20 10:48:22 +0100

    add scoreboard animation/styling for flashcards

--- flashcard-app/src/components/Score/index.jsx
+++ flashcard-app/src/components/Score/index.jsx
@@ -2,9 +2,8 @@ import React, { useState, useCallback } from "react";
 
 export default function Score({ score, totalQuestions, flaggedQuestions }) {
     const [clickedQuestions, setClickedQuestions] = useState([]);
-
     const percentage = (parseInt((score / totalQuestions) * 100));
-
+    
     const handleQuestionClick = useCallback(
         (index) => {
             if (clickedQuestions.includes(index)) {
@@ -22,8 +21,15 @@ export default function Score({ score, totalQuestions, flaggedQuestions }) {
     );
 
     const renderQuestion = (item, index) => (
-        <div key={index} onClick={() => handleQuestionClick(index)}>
-            <p>{clickedQuestions.includes(index) ? item.answer : item.question}</p>
+        <div key={index} className="flagged-question" onClick={() => handleQuestionClick(index)}>
+            <div className={`card ${clickedQuestions.includes(index) ? "flipped" : ""}`}>
+                <div className="card-face card-face-front">
+                    <p>{item.question}</p>
+                </div>
+                <div className="card-face card-face-back">
+                    <p>{item.answer}</p>
+                </div>
+            </div>
         </div>
     );
 
@@ -31,7 +37,10 @@ export default function Score({ score, totalQuestions, flaggedQuestions }) {
         <div>
             <h2>Score: {percentage}%</h2>
             <h3>Flagged Questions:</h3>
-            {flaggedQuestions.map(renderQuestion)}
+            <div className="flagged-questions-container">
+                {flaggedQuestions.map(renderQuestion)}
+            </div>
         </div>
     );
+
 }
