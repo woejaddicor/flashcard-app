29278f40ef623460b2819ee676a66ce3d9ab42d1
sk.ahm <itzshakeey@gmail.com>
2023-04-20 22:23:57 +0100

    apply changes to page

--- flashcard-app/src/pages/SpanishPage/index.jsx
+++ flashcard-app/src/pages/SpanishPage/index.jsx
@@ -6,7 +6,7 @@ import Button from 'react-bootstrap/button';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Popover from 'react-bootstrap/Popover';
 
-export default function SpanishPage({ testing=false, token, setToken }) {
+export default function SpanishPage({ testing = false, token, setToken }) {
     const [question, setQuestion] = useState([]);
     const [index, setIndex] = useState(1);
     const [quizFinished, setQuizFinished] = useState(false);
@@ -20,18 +20,18 @@ export default function SpanishPage({ testing=false, token, setToken }) {
 
     const popover = (
         <Popover id="popover-basic" >
-          <Popover.Header as="h3">How to play:</Popover.Header>
-          <Popover.Body>
-             To play the game <strong style={{color: "#ECA400"}}>hover over the blue card with the English text</strong>. When you think you know the correct translation, click the card to flip it over and see. <strong style={{color: "#ECA400"}}>Click either the 'right' or 'wrong' button depending on the outcome</strong> and your score will total up at the end.
-          </Popover.Body>
+            <Popover.Header as="h3">How to play:</Popover.Header>
+            <Popover.Body>
+                To play the game <strong style={{ color: "#ECA400" }}>hover over the blue card with the English text</strong>. When you think you know the correct translation, click the card to flip it over and see. <strong style={{ color: "#ECA400" }}>Click either the 'right' or 'wrong' button depending on the outcome</strong> and your score will total up at the end.
+            </Popover.Body>
         </Popover>
-      );
-      
-      const Instructions = () => (
+    );
+
+    const Instructions = () => (
         <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-          <Button className="instructions-button" variant="success">Instructions</Button>
+            <Button className="instructions-button" variant="success">Instructions</Button>
         </OverlayTrigger>
-      );
+    );
 
     useEffect(() => {
         async function fetchData() {
@@ -43,12 +43,10 @@ export default function SpanishPage({ testing=false, token, setToken }) {
 
             setTotalQuestions(maxQuestion.questions.length);
             setQuestion(data.question);
-
-            console.log()
-            
         }
         fetchData();
     }, [index]);
+
 
     function handlePreviousClick() {
         setIndex((prevIndex) => (prevIndex > 1 ? prevIndex - 1 : prevIndex));
@@ -85,29 +83,37 @@ export default function SpanishPage({ testing=false, token, setToken }) {
     return (
         <div>
             <Instructions />
-            <h1 className="french-page-title">Translate the word</h1>
-            <FlashcardNavigator
-                currentIndex={index}
-                onPreviousClick={handlePreviousClick}
-                onNextClick={handleNextClick}
-                totalQuestions={totalQuestions}
-                isLastQuestion={index === totalQuestions}
-            >
+            <div className="flashcard-app">
+                <h1 className="french-page-title">Translate the word</h1>
                 {question.map((phrase) => (
                     <Flashcard key={phrase.id} phrase={phrase} />
                 ))}
+
                 <button className="wrong-btn" onClick={handleWrongClick} disabled={clickedIndices.includes(index)}>Review flashcard</button>
-    
+                <div className="question-count">
+                    Word {index} of {totalQuestions}
+
+
+
+                </div>
+
                 {index === totalQuestions && (
                     <button className="finish-btn" onClick={handleFinishClick}>
                         Finish
                     </button>
                 )}
-            </FlashcardNavigator>
-            
-            <div className="question-count">
-                Word {index} of {totalQuestions}
+
+                <FlashcardNavigator
+                    currentIndex={index}
+                    onPreviousClick={handlePreviousClick}
+                    onNextClick={handleNextClick}
+                    totalQuestions={totalQuestions}
+                    isLastQuestion={index === totalQuestions}
+                >
+                </FlashcardNavigator>
+
             </div>
         </div>
-    )
+    );
+
 }
