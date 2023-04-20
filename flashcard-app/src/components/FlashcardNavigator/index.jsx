6915e51b76088162adc7d742e95f9ce0045c999a
Julia Barbosa <juliadealencarb@gmail.com>
2023-04-20 10:01:10 +0100

    Merging staging into julia.

--- flashcard-app/src/components/FlashcardNavigator/index.jsx
+++ flashcard-app/src/components/FlashcardNavigator/index.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
-// export default function FlashcardNavigator({ currentIndex, onPreviousClick, onNextClick, onFinishClick, disableNext, totalQuestions, children, }) {
-//     const isLastQuestion = currentIndex === totalQuestions;
-
-=======
->>>>>>> staging
 export default function FlashcardNavigator({ currentIndex, onPreviousClick, onNextClick, totalQuestions, children, isLastQuestion,
 }) {
     return (
